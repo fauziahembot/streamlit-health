9b897c28bf8abc8e15ea2c71ec0d645c27113c03
fauziahembot <119109378+fauziahembot@users.noreply.github.com>
2022-12-08 13:45:44 +0700

    Update stream-health.py

--- stream-health.py
+++ stream-health.py
@@ -9,33 +9,33 @@ st.title('Prediksi Kesehatan Ibu Ketika Sedang Mengandung (Hamil)')
 col1, col2 = st.columns(2)
 
 with col1:
-    Age = st.text_input('Masukan Umur')
+    Age = st.number_input('Umur')
 
 with col2:
-    SystolicBP = st.text_input('Masukan Nilai Tekanan Darah Yang Lebih Tinggi Dalam mmHg') 
+    SystolicBP = st.number_input('Nilai Tekanan Darah Yang Lebih Tinggi Dalam mmHg') 
 
 with col1:
-    DiastolicBP = st.text_input('Masukan Nilai Tekanan Darah Yang Lebih Rendah Dalam mmHg')
+    DiastolicBP = st.number_input('Nilai Tekanan Darah Yang Lebih Rendah Dalam mmHg')
 
 with col2:
-    BS = st.number_input('Masukan Nilai Kadar Glukosa Dalam Darah Dalam mmol/L')
+    BS = st.number_input('Nilai Kadar Glukosa Dalam Darah Dalam mmol/L')
 
 with col1:
-    BodyTemp = st.text_input('Masukan Nilai Temperatur Badan')
+    BodyTemp = st.number_input('Nilai Temperatur Badan')
 
 with col2:
-    HeartRate = st.text_input('Masukan Nilai Detak Jantung Dalam Keadaan Normal')
+    HeartRate = st.number_input('Nilai Detak Jantung Dalam Keadaan Normal')
 
-health_diagnosis = ''
+maternal_diagnosis = ''
 
 if st.button('Prediksi Kesehatan Ibu'):
-    health_prediction = model.predict([[Age, SystolicBP,  DiastolicBP, BS, BodyTemp, HeartRate]])
+    maternal_prediction = model.predict([[Age, SystolicBP,  DiastolicBP, BS, BodyTemp, HeartRate]])
 
-    if(health_prediction[0]==0):
-        health_diagnosis = 'Tingkat Resiko Rendah'
-    elif(health_prediction[0]==1):
-        health_diagnosis = 'Tingkat Resiko Sedang'
+    if(maternal_prediction[0]==0):
+        maternal_diagnosis = 'Tingkat Resiko Rendah'
+    elif(maternal_prediction[0]==1):
+        maternal_diagnosis = 'Tingkat Resiko Sedang'
     else:
-        health_diagnosis = 'Tingkat Resiko Tinggi'
+        maternal_diagnosis = 'Tingkat Resiko Tinggi'
 
-st.success(health_diagnosis)
+st.success(maternal_diagnosis)
